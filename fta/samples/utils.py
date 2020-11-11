99aa1ee5c5fd952f1b813692446c641e5ae45a9e
Sarah Bird <birdsarah@mozilla.com>
2020-11-11 14:20:29 -0600

    Add train/test/validation split

--- fta/samples/utils.py
+++ fta/samples/utils.py
@@ -1,3 +1,5 @@
+import random
+
 from bs4 import BeautifulSoup
 
 suffixes = ["B", "KB", "MB", "GB", "TB", "PB"]
@@ -25,3 +27,35 @@ def convert_labeled_sample_to_fathom_sample(labeled_sample, suffix=".html"):
             for element in tagged_elements:
                 element.attrs["data-fathom"] = label.label.slug
     return soup.encode("utf-8"), suffix
+
+
+def get_splits_from_queryset(qs, train_pct, test_pct):
+    # Returns train/test/validation splits from a queryset.
+    # Validation set is the remaining once train and test are picked.
+    # If train_pct and test_pct add to 100%, validation may still have 1
+    # item in it.
+    #
+    # This isn't perfect, it'll do for now.
+    #
+    # If you have too few samples, this is going to get wierd
+    if train_pct + test_pct > 1.0:
+        raise RuntimeError(
+            f"train_pct ({train_pct}) + test_pct ({test_pct}) must sum to less than 1.0."
+        )
+    n = qs.count()
+    n_train = round(train_pct * n)
+    n_test = round(test_pct * n)
+
+    # Warning: this has the potential to get inefficient, but should be fine while we're
+    # only doing modest samples.
+    qs_set = set(qs)
+    train_set = set(random.sample(qs_set, n_train))
+    remaining = qs_set - train_set
+    test_set = set(random.sample(remaining, n_test))
+    validation_set = remaining - test_set
+
+    return {
+        "training": train_set,
+        "testing": test_set,
+        "validation": validation_set,
+    }
