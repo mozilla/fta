38842b91b278b4caa7ee4d558363cecd302077a6
gleonard-m <gleonard@mozilla.com>
2020-12-17 14:17:18 -0500

    Issue 19 & 61

--- config/api_router.py
+++ config/api_router.py
@@ -1,7 +1,7 @@
 from django.conf import settings
 from rest_framework.routers import DefaultRouter, SimpleRouter
 
-from fta.samples.api.views import AddSampleViewSet, SampleViewSet
+from fta.samples.api.views import AddSampleViewSet, SampleViewSet, AddFathomSampleViewSet
 
 if settings.DEBUG:
     router = DefaultRouter()
@@ -11,6 +11,7 @@ else:
 # Example how to register
 router.register("samples", SampleViewSet)
 router.register("add_sample", AddSampleViewSet, basename="add_sample")
+router.register("add_fathom_sample", AddFathomSampleViewSet, basename="add_fathom_sample")
 
 
 app_name = "api"
--- fta/samples/api/views.py
+++ fta/samples/api/views.py
@@ -2,8 +2,9 @@ from rest_framework import mixins, viewsets
 from rest_framework.decorators import action
 from rest_framework.response import Response
 
-from ..models import Sample
+from ..models import Sample, LabeledSample, LabeledElement, Label
 from ..views import sample_from_required
+from ..utils import convert_fathom_sample_to_labeled_sample
 from .serializers import SampleListSerializer, SampleSerializer
 
 
@@ -43,4 +44,32 @@ class AddSampleViewSet(viewsets.ViewSet):
         notes = request.data["notes"] if "notes" in request.data else ""
         sample = sample_from_required(frozen_page, freeze_software, notes)
         sample.save()
+        return Response({"id": sample.id})
+
+
+class AddFathomSampleViewSet(viewsets.ViewSet):
+    basename = "add_fathom_sample"
+
+    @action(detail=False, methods=["post"])
+    def add_fathom_sample(self, request, format=None):
+        frozen_page = request.data["frozen_page"]
+        freeze_software = request.data["freeze_software"]
+        notes = request.data["notes"] if "notes" in request.data else ""
+        sample = sample_from_required(frozen_page, freeze_software, notes)
+        sample.save()
+
+        fta_sample, labels_to_fta_ids = convert_fathom_sample_to_labeled_sample(frozen_page)
+        labeled_sample, _ = LabeledSample.objects.get_or_create(
+                original_sample=sample,
+                modified_sample=fta_sample
+        )
+
+        for fathom_label, fta_id in labels_to_fta_ids.items():
+            stored_label, created = Label.objects.get_or_create(slug=fathom_label)
+            LabeledElement.objects.get_or_create(
+                labeled_sample=labeled_sample,
+                data_fta_id=fta_id,
+                label=stored_label,
+            )
+
         return Response({"id": sample.id})
